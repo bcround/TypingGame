cb5b7b826b79cca53fdf213daa362f35340994c8
bcround <sbc1142@gmail.com>
2021-01-12 17:26:53 +0900

    feat: Doesn't generate duplicate of words anymore

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const words = ["vulnerable", "invulnerable", "mandate",
 "profitable", "nerve", "substantive", "candid", "candor", 
 "dormant", "guilty", "equivocal", "gloom", "evident", 
 "budget","convince", "assume", "reputation"];
+let timeLeftCount;
 
 // DOM
 const $timeLeft = document.querySelector('.time-left');
@@ -14,14 +15,20 @@ const $finalScore = document.querySelector('.final-score');
 const shuffleWords = () => {
   const randomPick = Math.floor(Math.random() * words.length);
 
+  if (!words.length) {
+    finishGame();
+    window.alert('You finished all!');
+  }
+
   $word.textContent = words[randomPick];
+  words.splice(randomPick, 1);
 };
 
 const compareWords = word => {
   if (word === $word.textContent) {
-    shuffleWords();
     ++$currentScore.lastElementChild.textContent;
     $timeLeft.textContent = +$timeLeft.textContent + 2;
+    shuffleWords();
   }
 };
 
@@ -30,15 +37,12 @@ const displayFinalScore = () => {
   $finalScore.lastElementChild.textContent = $currentScore.lastElementChild.textContent;
 };
 
-const timeLeftCount = setInterval(() => {
-  --$timeLeft.textContent;
-  if ($timeLeft.textContent === '0') {
-    clearInterval(timeLeftCount);
-    $inputWord.setAttribute('disabled','');
-    displayFinalScore();
-    window.alert('Times Up!');
-  }
-}, 1000);
+const finishGame = () => {
+  clearInterval(timeLeftCount);
+  $inputWord.setAttribute('disabled', '');
+  displayFinalScore();
+  $timeLeft.textContent = 0;
+};
 
 $inputWord.addEventListener('keyup', e => {
   const word = e.target.value;
@@ -47,3 +51,18 @@ $inputWord.addEventListener('keyup', e => {
   compareWords(word);
   e.target.value = '';
 });
+
+const init = () => {
+  shuffleWords();
+  $timeLeft.textContent = 10;
+
+  timeLeftCount = setInterval(() => {
+    --$timeLeft.textContent;
+    if ($timeLeft.textContent === '0') {
+      finishGame();
+      window.alert('Times Up!');
+    }
+  }, 1000);
+};
+
+document.addEventListener('DOMContentLoaded', init);
